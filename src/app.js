d69d70afd328254b950cba6f71b7d24634696c15
Porramat Chairattanah <nanineprm@Porramats-MacBook-Air.local>
2023-05-25 10:32:10 +0700

    config all middleware

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+if (process.env.NODE_ENV === "develop") {
+  app.use(morgan("combined"));
+}
+
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 1000,
+    message: { message: "too many request" },
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("server running in port " + port));
